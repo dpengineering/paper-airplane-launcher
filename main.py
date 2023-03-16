8dad6962b3516d9659960eb1fb2695987fa86a47
Student <andrewhamers@dpengineering.org>
2023-03-16 14:50:52 -0700

    adding odrive_helpers

--- main.py
+++ main.py
@@ -1,30 +1,60 @@
+import sys
+
 from dpeaDPi.DPiComputer import DPiComputer
 from time import sleep
+from threading import Thread
+from odrive_helpers import *
+from odrive.utils import start_liveplotter
+
+"""Hardware Setup"""
+dpiComputer = DPiComputer()
+dpiComputer.initialize()
+
+clockDivisor = 360  # for hand turned encoders, slow the state machine
+dpiComputer.setEncoderClockDivisor(clockDivisor)  # to reduce noise, otherwise skip this operation
 
 
-def main():
-    #
-    # Create a DPiComputer object
-    #
-    dpiComputer = DPiComputer()
-
-    #
-    # Initialize the board to its default values
-    #
-    dpiComputer.initialize()
-
-    print("Encoder example:")
-    clockDivisor = 360  # for hand turned encoders, slow the state machine
-    dpiComputer.setEncoderClockDivisor(clockDivisor)  # to reduce noise, otherwise skip this operation
-
+def get_value():
     encoder_number = 1
-    for _i in range(50):
-        value = dpiComputer.readEncoder(encoder_number)
-        print("  Encoder position: " + str(value))
+    value = dpiComputer.readEncoder(encoder_number)
+    return value
+
+
+def print_info(ax: ODriveAxis):
+    print(f"Curr State: {ax.axis.current_state}")
+    print(f"Input Mode = {ax.axis.controller.config.input_mode}")
+    print(f"Control Mode: {ax.axis.controller.config.control_mode}")
+
+
+def encoder_action(ax: ODriveAxis):
+    while True:
+        x_val = get_value()
+        vel = x_val / 10
+        ax.set_vel(vel)
         sleep(.1)
 
-    print("All examples complete")
+
+def encoder_action_thread(ax):
+    Thread(target=encoder_action, args=(ax,), daemon=True).start()
 
 
 if __name__ == "__main__":
-    main()
+    od = find_odrive()
+
+    assert od.config.enable_brake_resistor is True, "Check for faulty brake resistor."
+
+    ax0 = ODriveAxis(od.axis0)
+    if not ax0.is_calibrated():
+        print("calibrating...")
+        ax0.calibrate()
+    ax0.set_gains()
+
+    try:
+        start_liveplotter(lambda: [ax0.axis.encoder.pos_estimate, ax0.axis.controller.input_pos, ])
+        encoder_action_thread(ax0)
+        while True:
+            sleep(10)
+    finally:
+        ax0.idle()
+        dump_errors(od)
+        print("DONE")
